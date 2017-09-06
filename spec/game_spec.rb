e9505a0eebef32eb0fbbc91789db51fcbf64534d
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-06 12:14:18 +0100

    Added tests for setting a field on the board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,28 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new }
-  let(:blank) { "   \n   \n   \n" }
 
   describe 'initialisation' do
     it 'starts with a blank board' do
-      expect { puts game }.to output(blank).to_stdout
+      expect { puts game }.to output("   \n   \n   \n").to_stdout
     end
 
     it 'says game is not over' do
       expect(game.isOver?).to be false
+    end
+  end
+
+  describe '#set_field' do
+    context 'field has not been previously set' do
+      it 'can set a field with X' do
+        game.set_field(1, 1, 'X')
+        expect { puts game }.to output("   \n X \n   \n").to_stdout
+      end
+
+      it 'can set a field with O' do
+        game.set_field(1, 2, 'O')
+        expect { puts game }.to output("   \n  O\n   \n").to_stdout
+      end
     end
   end
 end
